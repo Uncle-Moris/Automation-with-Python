36c4e6df5d681a9ab1932a9532241fd47c5b4141
maurycywoznica <mjw11@wp.pl>
2022-07-09 23:36:29 +0200

    First oop for selenium

--- some_selenium/OLX_offer/main.py
+++ some_selenium/OLX_offer/main.py
@@ -1,11 +1,25 @@
 from time import sleep
+import unittest
+import page
 
 from selenium.webdriver.common.by import By
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
 
-driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
-driver.get('https://www.olx.pl/')
-driver.find_element(By.XPATH, '//*[@id="onetrust-accept-btn-handler"]').click()
-sleep(300)
+
+class OLXSearch(unittest.TestCase):
+    def setUp(self):
+        self.driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+        self.driver.get('https://www.olx.pl/')
+
+    def test_search_in_OLX(self):
+        main_page = page.MainPage(self.driver)
+        search_bar = self.driver.find_element(By.XPATH, '//*[@id="onetrust-accept-btn-handler"]').click()
+
+
+
+
+driver.find_element(By.XPATH,'/html/body/div[1]/section[1]/form/div/fieldset/div[1]/div/div[1]/input').send_keys('Mieszkania')
+sleep(2)
+# 'https://twitter.com/Goaloo_sports'
