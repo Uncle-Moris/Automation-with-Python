db9ca24dc5810374417ef43bf76a725431f567d2
maurycywoznica <mjw11@wp.pl>
2022-07-11 22:24:36 +0200

    Some ... O dont know

--- some_selenium/GPW_checker/main.py
+++ some_selenium/GPW_checker/main.py
@@ -1,6 +1,6 @@
 from time import sleep
 import asyncio
-
+from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium import webdriver
@@ -18,9 +18,14 @@ def checking_the_share_price(name: str):
     #price = driver.find_elements(By.XPATH, '/html/body/section[2]/div[2]/div[5]/div/div[1]/div[1]/div[3]/div[1]/div[2]/table/tbody/tr')
     sleep(2)
     price_of_share = driver.find_element(By.XPATH, '//tbody/tr[1]/td[8]')
+    price_of_share.is_enabled()
     sleep(3)
     print(price_of_share.text)
     print(type(price_of_share))
+    current_window = driver.current_window_handle
+    window_names = driver.window_handles
+
+
     driver.quit()
 
-
+checking_the_share_price('pge')
--- some_selenium/OLX_offer/main.py
+++ some_selenium/OLX_offer/main.py
@@ -11,15 +11,18 @@ from webdriver_manager.chrome import ChromeDriverManager
 class OLXSearch(unittest.TestCase):
     def setUp(self):
         self.driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+        self.driver.maximize_window()
         self.driver.get('https://www.olx.pl/')
 
-    def test_search_in_OLX(self):
+    def test_search_in_OLX(self, name: str):
         main_page = page.MainPage(self.driver)
-        search_bar = self.driver.find_element(By.XPATH, '//*[@id="onetrust-accept-btn-handler"]').click()
+        search_bar = self.driver.find_element(By.XPATH, '//*[@id="onetrust-accept-btn-handler"]')
+        search_bar.send_keys(name)
+        search_bar.click()
+        sleep(5)
+        self.driver.quit()
 
 
 
-
-driver.find_element(By.XPATH,'/html/body/div[1]/section[1]/form/div/fieldset/div[1]/div/div[1]/input').send_keys('Mieszkania')
-sleep(2)
-# 'https://twitter.com/Goaloo_sports'
+if __name__ == '__main__':
+    OLXSearch.test_search_in_OLX(name='agava')
