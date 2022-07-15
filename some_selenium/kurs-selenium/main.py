48bb0168b23062925c220b190a10fd421f6c52e5
maurycywoznica <mjw11@wp.pl>
2022-07-15 23:24:27 +0200

    add kurs selenium

--- some_selenium/kurs-selenium/main.py
+++ some_selenium/kurs-selenium/main.py
@@ -8,19 +8,22 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support import expected_conditions as EC, expected_conditions
+from selenium.common.exceptions import NoSuchElementException
+
+
 
 @pytest.fixture
 def test_setup():
     global driver
+    global wait
     driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
     driver.maximize_window()
+    wait = WebDriverWait(driver, 10, 0.5, NoSuchElementException)
     driver.get('http://www.kurs-selenium.pl/demo/')
     yield
     driver.quit()
 
 
 def test_something(test_setup):
-    WebDriverWait(driver, 10).until(driver.find_element(By.XPATH, "/html/body/div[5]/div[1]/a[2]/div/div").click())
-    driver.implicitly_wait(10)
-
+    wait.until(expected_conditions.visibility_of_element_located((By.XPATH, "/html/body/div[5]/div[1]/a[2]/div/div")))
