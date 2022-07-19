3fe6b5b3a862a2ff5044d67dbf5b405777d5624b
maurycywoznica <mjw11@wp.pl>
2022-07-19 23:02:00 +0200

    some behave code

---
+++ some_behave/features/steps/some_senario.py
@@ -0,0 +1,21 @@
+from behave import given, when, then
+
+
+@given(u'lunch chrome browser')
+def step_impl(context):
+    assert True, "Test passed"
+
+
+@when(u'open homepage')
+def step_impl(context):
+    assert True, "Test passed"
+
+
+@then(u'verify chasing a first car')
+def step_impl(context):
+    assert True, "Test passed"
+
+
+@then(u'close browser')
+def step_impl(context):
+    assert True, "Test passed"
---
+++ some_selenium/GPW_checker/features/some_senario.feature
@@ -0,0 +1,9 @@
+# Created by maurycywoznica at 19/07/2022
+Feature: # Enter feature name here
+  # Enter feature description here
+
+  Scenario: # Enter scenario name here
+    Given lunch chrome browser
+    When open homepage
+    Then verify chasing a first car
+    And close browser
