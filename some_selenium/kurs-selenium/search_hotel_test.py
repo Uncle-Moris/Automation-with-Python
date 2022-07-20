a5c58f38b5a123a14fa8978f5e66bd4f9372cf9a
maurycywoznica <mjw11@wp.pl>
2022-07-20 23:46:39 +0200

    Clear

--- some_selenium/kurs-selenium/search_hotel_test.py
+++ some_selenium/kurs-selenium/search_hotel_test.py
@@ -19,20 +19,5 @@ driver.find_element_by_xpath()
 driver.find_element_by_name()
 driver.find_element_by_name()
 
-'''
-@pytest.fixture
-def test_setup():
-    global driver
-    global wait
-    driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
-    driver.maximize_window()
-    wait = WebDriverWait(driver, 10, 0.5, NoSuchElementException)
-    driver.get('http://www.kurs-selenium.pl/demo/')
-    yield
-    driver.quit()
 
-
-def test_something(test_setup):
-    wait.until(expected_conditions.visibility_of_element_located((By.XPATH, "/html/body/div[5]/div[1]/a[2]/div/div")))
-'''
 
