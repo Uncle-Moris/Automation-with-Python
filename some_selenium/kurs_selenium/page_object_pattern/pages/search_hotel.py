6bc2cd99178af2ee93d3a839755b55506d2d4585
maurycywoznica <mjw11@wp.pl>
2022-07-24 20:52:56 +0200

    add logs to test

--- some_selenium/kurs_selenium/page_object_pattern/pages/search_hotel.py
+++ some_selenium/kurs_selenium/page_object_pattern/pages/search_hotel.py
@@ -1,9 +1,12 @@
 from selenium.webdriver.common.by import By
+import logging
 
 class SearchHotelPage:
 
     def __init__(self, driver):
         self.driver = driver
+        self.logger = logging.getLogger(__name__)
+
         self.search_hotel_span_xpath = "//span[text()='Search by Hotel or City Name']"
         self.search_hotel_input_xpath = "//div[@id='select2-drop']//input"
         self.locating_mach_xpath = "//span[text()='Dubai']"
@@ -15,15 +18,18 @@ class SearchHotelPage:
         self.search_button_xpath = "//button[text()=' Search']"
 
     def set_city(self, city):
+        self.logger.info(f'Setting city - {city}')
         self.driver.find_element(By.XPATH, self.search_hotel_span_xpath).click()
         self.driver.find_element(By.XPATH, self.search_hotel_input_xpath).send_keys(city)
         self.driver.find_element(By.XPATH, self.locating_mach_xpath).click()
 
     def set_date_range(self, check_in, check_out):
+        self.logger.info(f'Setting check in - {check_in} and check out {check_out}')
         self.driver.find_element(By.NAME, self.check_in_input_name).send_keys(check_in)
         self.driver.find_element(By.NAME, self.check_out_input_name).send_keys(check_out)
 
     def set_travellers(self, adults, child):
+        self.logger.info(f'Setting travellers adults {adults} kind {child}')
         self.driver.find_element(By.ID, self.travellers_input_id).click()
         self.driver.find_element(By.ID, self.adult_input_id).clear()
         self.driver.find_element(By.ID, self.adult_input_id).send_keys(adults)
@@ -31,4 +37,5 @@ class SearchHotelPage:
         self.driver.find_element(By.ID, self.child_input_id).send_keys(child)
 
     def perform_search(self):
+        self.logger.info('Perform search')
         self.driver.find_element(By.XPATH, self.search_button_xpath).click()
--- some_selenium/kurs_selenium/page_object_pattern/tests/test_hotel_search.py
+++ some_selenium/kurs_selenium/page_object_pattern/tests/test_hotel_search.py
@@ -2,7 +2,7 @@ import pytest
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from some_selenium.kurs_selenium.page_object_pattern.pages.search_hotel import SearchHotelPage
-
+from some_selenium.kurs_selenium.page_object_pattern.pages.search_results import SearchResultsPage
 
 class TestHotelSearch:
 
@@ -21,3 +21,16 @@ class TestHotelSearch:
         search_hotel_page.set_date_range("12/09/2022", "15/09/2022")
         search_hotel_page.set_travellers("2", "2")
         search_hotel_page.perform_search()
+        result_page = SearchResultsPage(self.driver)
+        hotel_names = result_page.get_hotel_names()
+        prices_values = result_page.get_hotel_prices()
+
+        assert hotel_names[0] == 'Jumeirah Beach Hotel'
+        assert hotel_names[1] == 'Oasis Beach Tower'
+        assert hotel_names[2] == 'Rose Rayhaan Rotana'
+        assert hotel_names[3] == 'Hyatt Regency Perth'
+
+        assert prices_values[0] == '$22'
+        assert prices_values[1] == '$50'
+        assert prices_values[2] == '$80'
+        assert prices_values[3] == '$150'
